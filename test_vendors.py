6adb0a44bb9a335cb73ec097fd31b7ae6f382349
Louis <louismarsaud@gmail.com>
2023-08-17 14:19:57 +0200

    [MSI LAPTOP] Update test vendor Houdini lab camera conform script

--- test_vendors.py
+++ test_vendors.py
@@ -1,10 +1,12 @@
-####RUN THIS CODE IN CLEAN PYTHON INSTALL ###
+#### RUN THIS CODE IN CLEAN PYTHON INSTALL #####
 
 import sys
 import os
+
 root_package = os.getcwd()
 
 if not root_package in sys.path:
+
     sys.path.append(root_package)
 
 import vendor
@@ -12,13 +14,16 @@ import vendor
 
 from vendor import yaml
 
-test_file = os.path.join(root_package,'config','test.yaml')
+test_file = os.path.join(root_package, 'config', 'test.yaml')
 
 with open(test_file) as f:
+
     data = yaml.load(f, Loader=yaml.FullLoader)
     print(data)
 
 try :
     from vendor import Qt
+
 except :
-    print('If no binding installed Qt.py raise and error please install one Qt binding, PySide2 or PySide6, PyQt4 or PyQt5, etc...')
+    print('If no binding installed Qt.py raise and error please install one Qt binding, '
+          'PySide2 or PySide6, PyQt4 or PyQt5, etc...')
