2f2077a2a952e1ca54e18db28222c757aa8f28ae
lmrsd <louismarsaud@gmail.com>
2023-08-24 19:58:24 +0200

    [YARD 56]
    Manager render farm

--- __init__.py
+++ __init__.py
@@ -1,18 +1,2 @@
 import os
 import logging
-
-# Try import PyAml if not installed
-try :
-    import yaml
-except :
-    import VfxToolset.vendor.yaml
-    logging.info("VfxToolSet : yaml module imported")
-
-
-# Try import Qt.py if not existting
-try :
-    import Qt
-except :
-    import VfxToolset.vendor.Qt
-    logging.info("VfxToolSet : Qt  module imported")
-
