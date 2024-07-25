b715e206881a6346f815260d3afd6811068804c7
Louis.Mrsd <louismarsaud@gmail.com>
2024-07-25 22:00:09 +0200

    [MSI LAPTOP] Update New import method for studio in general , start refactor vfx browser

--- __init__.py
+++ __init__.py
@@ -1,6 +1,15 @@
-print("CG Tool setImported")
+import os
+import json
 
 class Studio():
-    def __init__(self,studio_name):
-        print("Initialize studio")
-        print(studio_name)
+    def __init__(self, studio_name, mod_root):
+        print("Initialize Studio : {}".format(studio_name))
+
+        self.studio_name = studio_name
+        self.root = mod_root
+
+        self.c_path = os.path.join(mod_root, 'CGToolset', 'config', '{}.json'.format(studio_name) )
+
+        if os.path.exists(self.c_path):
+            with open(self.c_path, 'r') as f:
+                self.config = json.load(f)
